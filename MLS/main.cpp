3faa805e88a56069847fbb387db652ef288aa51e
Mayank Raj <mayankraj@mayanks-macbook-pro.local>
2018-11-11 19:10:13 -0600

    Initial Commit

---
+++ MLS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MLS
+//
+//  Created by Mayank Raj on 11/11/18.
+//  Copyright © 2018 Mayank Raj. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
